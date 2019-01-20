5f94e6af2e8ecafd0c43595a102de5e3db6802e5
pi <pi@raspberry.com>
2019-01-20 20:28:00 +0500

    Multiple sensors measures + InfluxDB bind + Grafana setup is now working

--- DS18B20/main.py
+++ DS18B20/main.py
@@ -36,8 +36,21 @@ os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
 base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
+device_folder = glob.glob(base_dir + '28*')
+
+nb_device = len(device_folder)
+print "Number of devives: {0}".format(nb_device)
+
+# Creates a list containing device list with  ['Directory folder, "type", "serial"]
+device = [[0 for x in range(3)] for y in range(nb_device)] 
+for x in range(0,nb_device):
+  print(x)
+  device[x][0] = device_folder[x] + '/w1_slave'
+  device[x][1] = "Temperature"
+  device[x][2] = device_folder[x].replace(base_dir, '')
+  print(device[x][0])
+  print(device[x][1])
+  print(device[x][2])
 
 if INFLUX_ENABLE == 'yes':
   from influxdb import InfluxDBClient
@@ -56,11 +69,11 @@ def convert_to_f(temp_c):
   '''
   return temp_c * 9.0 / 5.0 + 32.0
 
-def read_ext_temp():
+def read_ext_temp(sensor_file):
   '''
   Read temps from DS18B20 thermometer
   '''
-  f = open(device_file, 'r')
+  f = open(sensor_file, 'r')
   lines = f.readlines()
   f.close()
   temp_line = [l for l in lines if 't=' in l]
@@ -69,26 +82,27 @@ def read_ext_temp():
 
 try:
   while True:
-    ext_temp = read_ext_temp()
-    print "EXTERNAL: Temp C: {0} - Temp F: {1}".format(ext_temp, convert_to_f(ext_temp))
+    for x in range(0,nb_device):
+        ext_temp = read_ext_temp(device[x][0])
+        print "Serial:{0} --  Type:{1} --  Temp C: {2} -- Temp F: {3}".format(device[x][2],device[x][1],ext_temp, convert_to_f(ext_temp))
 
-    if INFLUX_ENABLE == 'yes':
-      # Format JSON for sending to InfluxDB
-      current_time = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
-      json_body = [{
-        "measurement": "temperature",
-        "tags": {
-            "sensor": "TODO",
-            "type": "TODO"
-        },
-        "time": current_time,
-        "fields": {
-            "value": ext_temp
-        }
-      }]
+        if INFLUX_ENABLE == 'yes':
+          # Format JSON for sending to InfluxDB
+          current_time = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+          json_body = [{
+            "measurement": "temperature",
+            "tags": {
+                "serial": device[x][2],
+                "type": device[x][1]
+            },
+            "time": current_time,
+            "fields": {
+                "value": ext_temp
+           }
+          }]
 
-      # Send data to InfluxDB
-      idb_client.write_points(json_body)
+          # Send data to InfluxDB
+          idb_client.write_points(json_body)
 
     # Take a nap for a sec
     time.sleep(1)
