92b1c2e2ea9f01bdcc216728c5f3b2b29a7ce076
Donavan_Martin <donavanmartin@algorithmesolutions.com>
2019-03-12 12:24:02 -0400

    Update install.sh

--- Grafana/install.sh
+++ Grafana/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo apt-get install adduser libfontconfig
-curl -L https://dl.bintray.com/fg2it/deb-rpi-1b/main/g/grafana_4.0.2-1481228314_armhf.deb -o /tmp/grafana_4.0.2-1481228314_armhf.deb
-sudo dpkg -i /tmp/grafana_4.0.2-1481228314_armhf.deb
+curl -L https://dl.bintray.com/fg2it/deb-rpi-1b/main/g/grafana_5.1.4_armhf.deb -o /tmp/grafana_5.1.4_armhf.deb
+sudo dpkg -i /tmp/grafana_5.1.4_armhf.deb
 sudo systemctl daemon-reload
 sudo systemctl enable grafana-server && sudo systemctl start grafana-server
+
+# Grafana Raspberry pi Release : https://dl.bintray.com/fg2it/deb-rpi-1b/main/g/
